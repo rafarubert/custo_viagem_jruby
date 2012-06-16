6ca77d12f0d0f299f39ddabc1495e8fc52362c41
Rafael Cruz Rubert <rafarubert@yahoo.com.br>
2012-06-16 04:00:42 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustoViagem::Application.config.secret_token = 'e9bbb668f02ce304a55af508dd20a430ced39852bedc302c42db0b5161f0dd96a303bb3f16cafdca591ac8f234c460ee13fd7215f697188ec1e9235fcd8eb065'
