0f59302200bac142abbcf7c67eec6aa3893c50f3
Victor Sobreira <victorcsv@gmail.com>
2012-06-17 00:48:03 -0300

    Finishi merda

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,20 +7,13 @@ class TripsController < ApplicationController
   end
 
   def create
-    @trip = Trip.new(params[:trip])
-    
-    respond_to do |wants|
-      if @trip.save
-        # begin
-          @passages = Passage.find(@trip.origin, @trip.destination, @trip.initial_date, @trip.final_date)
-          @hotels = Hotel.find_by_city(Airport.value_for(@trip.destination))
-        # rescue Exception => e
-        #   puts e.inspect
-        #   render :error
-        # end
-      else
-        
-      end
+    @trip.save
+    begin
+      @passages = Passage.find(@trip.origin, @trip.destination, @trip.initial_date, @trip.final_date)
+      @hotels = Hotel.find_by_city(Airport.value_for(@trip.destination))
+    rescue Exception => e
+    #   puts e.inspect
+      render :error
     end
   end
 
