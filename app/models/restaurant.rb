d0771b8ff7764b19130bb6d25a67fe53fe85b698
Rafael Cruz Rubert <rafarubert@yahoo.com.br>
2012-06-16 21:18:43 -0300

    Add search to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,22 +1,47 @@
 require 'open-uri'
-require 'json'
+require 'nokogiri'
 
 class Restaurant
-
+  #parse: fortaleza or porto-alegre
+  #TODO: fix page status 404
   def find_by_city(city = String.new)
-    url = "http://vejabrasil.abril.com.br/portal/services/search/estab/?rg=#{city}&chn=restaurantes"
+    url = "http://vejabrasil.abril.com.br/#{city}/busca/?area=estabelecimentos&tipo=restaurantes"
+
     build = Hash.new
 
     name = String.new
     price = String.new
 
-    buffer = open(url, "UserAgent" => "Ruby-Wget").read
+    document = Nokogiri::HTML(open(url))
 
-    result = JSON.parse(buffer)
+    document.css('#results > .box').each do |doc|
+      doc.children.css('h3').each do |restaurant|
+        name = restaurant.text.gsub!(" - #{restaurant.children.css('span').text}", "")
 
-    result['navigators'].each do |navigator|
-      puts navigator.inspect
+        restaurant_document  = document = Nokogiri::HTML(open(restaurant.children.css('a').attribute('href')))
+        restaurant_document.css("div.caixa").each do |restaurant_values|
+          restaurant_values.children.css('ul > li:last > b').remove
+          restaurant_values.children.css('ul > li:last > strong').remove
+          price = restaurant_values.children.css('ul > li:last').text
+          price = (/\(de R\$(.+)\sa\sR\$(.+)\)/).match(price).to_s
+
+          unless price.blank?
+            price.gsub!('(de R$ ','')
+            price.gsub!(')','')
+            price.gsub!('R$ ','')
+            price.gsub!('a','+')
+            price.gsub!(',','.')
+            price = eval("#{price}/2")
+            price = ActionController::Base.helpers.number_to_currency(price, :unit => "R$", :separator => ",",:delimiter => ".")
+
+            if name and price
+              build[name] = price
+            end
+          end
+        end
+      end
     end
-    return false
+
+    build
   end
 end
